e958e920eae34a93e3bb6d191d0a1955ddafc356
mghezel61 <45494287+mghezel61@users.noreply.github.com>
2022-02-13 18:46:07 +0100

    refactor the script.js

--- js/script.js
+++ js/script.js
@@ -11,12 +11,14 @@ hamBtn.addEventListener("click", toggle);
 navLinks.forEach((link) => {
   link.addEventListener("click", function (e) {
     e.preventDefault();
-    toggle();
+    // prevent add the show class in Desktop Mode
+    if (window.innerWidth < 876) {
+      toggle();
+    }
     let TargetSecHeight = body.querySelector(
       link.attributes[0].value
     ).offsetTop;
     window.scroll({ top: TargetSecHeight, left: 0, behavior: "smooth" });
-    console.dir(TargetSecHeight);
   });
 });
 // toggle function
@@ -43,14 +45,9 @@ function slideTop() {
     window.scroll({ top: 0, left: 0, behavior: "smooth" });
   });
   // hide the header in Desktop mode
-  if (currentScrollPos > 200) {
-    header.classList.add("hide");
-  }
-  if (currentScrollPos < lastYPos) {
-    header.classList.remove("hide");
-  }
+  if (currentScrollPos > 200) header.classList.add("hide");
+  if (currentScrollPos < lastYPos) header.classList.remove("hide");
   lastYPos = currentScrollPos;
-  // console.log("current", currentScrollPos);
 }
 // ##################################################
 
