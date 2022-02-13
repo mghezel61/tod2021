ea97370629228fd5b01edcf98b3b35b4d20e1278
mghezel61 <45494287+mghezel61@users.noreply.github.com>
2022-02-13 17:27:08 +0100

    add the scroll animation

--- js/script.js
+++ js/script.js
@@ -4,11 +4,21 @@ let mainNav = document.querySelector("#main_nav");
 let navLinks = mainNav.querySelectorAll("a");
 let sliderToTop = document.querySelector("#scrollToTop");
 let body = document.querySelector("body");
-console.log(navLinks);
+let header = body.querySelector("header");
 
 // toggle the navigation #########################
 hamBtn.addEventListener("click", toggle);
-navLinks.forEach((link) => link.addEventListener("click", toggle));
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    toggle();
+    let TargetSecHeight = body.querySelector(
+      link.attributes[0].value
+    ).offsetTop;
+    window.scroll({ top: TargetSecHeight, left: 0, behavior: "smooth" });
+    console.dir(TargetSecHeight);
+  });
+});
 // toggle function
 function toggle() {
   mainNav.classList.toggle("show");
@@ -18,8 +28,10 @@ function toggle() {
 }
 // ###############################################
 
-// scroll the page to the top ######################
+// scroll the page to the top /TOGGLE THE NAVBAR IN DESKTOP MODE ######################
+let lastYPos = 0;
 window.addEventListener("scroll", slideTop);
+// console.log("last", lastYPos);
 function slideTop() {
   let currentScrollPos = window.scrollY;
   if (currentScrollPos > 20) {
@@ -30,6 +42,16 @@ function slideTop() {
   sliderToTop.addEventListener("click", function () {
     window.scroll({ top: 0, left: 0, behavior: "smooth" });
   });
+  // hide the header in Desktop mode
+  if (currentScrollPos > 200) {
+    header.classList.add("hide");
+  }
+  if (currentScrollPos < lastYPos) {
+    header.classList.remove("hide");
+  }
+  lastYPos = currentScrollPos;
+  // console.log("current", currentScrollPos);
 }
-
 // ##################################################
+
+//
