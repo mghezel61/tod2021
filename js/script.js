52410393fb9fccdf0a983cf53db6889961cf8bf7
mghezel61 <45494287+mghezel61@users.noreply.github.com>
2022-02-13 16:44:54 +0100

    finilze the landing page

--- js/script.js
+++ js/script.js
@@ -2,14 +2,13 @@ let hamBtn = document.querySelector("#hamburger_btn");
 let hamBtnSpans = hamBtn.querySelectorAll("span");
 let mainNav = document.querySelector("#main_nav");
 let navLinks = mainNav.querySelectorAll("a");
+let sliderToTop = document.querySelector("#scrollToTop");
+let body = document.querySelector("body");
 console.log(navLinks);
 
-// toggle the navigation
+// toggle the navigation #########################
 hamBtn.addEventListener("click", toggle);
-navLinks.forEach((link) => {
-  link.addEventListener("click", toggle);
-});
-
+navLinks.forEach((link) => link.addEventListener("click", toggle));
 // toggle function
 function toggle() {
   mainNav.classList.toggle("show");
@@ -17,3 +16,20 @@ function toggle() {
     span.classList.toggle("clicked");
   });
 }
+// ###############################################
+
+// scroll the page to the top ######################
+window.addEventListener("scroll", slideTop);
+function slideTop() {
+  let currentScrollPos = window.scrollY;
+  if (currentScrollPos > 20) {
+    sliderToTop.classList.add("up");
+  } else {
+    sliderToTop.classList.remove("up");
+  }
+  sliderToTop.addEventListener("click", function () {
+    window.scroll({ top: 0, left: 0, behavior: "smooth" });
+  });
+}
+
+// ##################################################
